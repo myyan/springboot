3ecf92af3dc99b9c62b1951c3274adc8f7bae58e
unknown <heiqie@wacai.com>
2017-03-02 16:39:07 +0800

    add a controller to project

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.controller;
+
+import com.example.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by heiqie on 2017/3/2.
+ */
+@Controller
+public class UserController {
+
+    Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @RequestMapping("/")
+    @ResponseBody
+    public User home() {
+        User user = new User();
+        user.setId(1);
+        user.setName("user");
+        user.setAge(12);
+        log.info("user :{}",user);
+        return user;
+    }
+}
